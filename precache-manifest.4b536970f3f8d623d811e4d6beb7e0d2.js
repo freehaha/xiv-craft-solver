3d1a0afeb8b15fdc778290cb46a36c38f02a6e4a
Freehaha <freehaha@gmail.com>
2020-03-20 18:41:48 +1100

    Updates

---
+++ precache-manifest.4b536970f3f8d623d811e4d6beb7e0d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e81ea37955008881c90ffc6131da149b",
+    "url": "/xiv-craft-solver/index.html"
+  },
+  {
+    "revision": "c8139657f6e8b0d69fa9",
+    "url": "/xiv-craft-solver/static/css/main.01fa5c35.chunk.css"
+  },
+  {
+    "revision": "1f046ac080656f599b2d",
+    "url": "/xiv-craft-solver/static/js/2.02c6b747.chunk.js"
+  },
+  {
+    "revision": "a108088b0317a4b3eaee2a12a54f856c",
+    "url": "/xiv-craft-solver/static/js/2.02c6b747.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8139657f6e8b0d69fa9",
+    "url": "/xiv-craft-solver/static/js/main.77e28400.chunk.js"
+  },
+  {
+    "revision": "a31a30d9a3d4a35f63ee",
+    "url": "/xiv-craft-solver/static/js/runtime-main.302411a6.js"
+  }
+]);
